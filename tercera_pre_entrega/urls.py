9fd7f920c234652a7ee5d296abab226205fa5fbd
Irina Greseva <iagreseva@gmail.com>
2023-06-28 19:28:27 -0300

    Cree un projecto de tercer pre entrega

---
+++ tercera_pre_entrega/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for tercera_pre_entrega project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+#from tercera_pre_entrega.views import inicio,segunda_vista,fecha_actual
+from tercera_pre_entrega import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path ('',views.inicio),
+    path ('segunda-vista/',views.segunda_vista),
+    path ('fecha-actual/',views.fecha_actual),
+    path ('saludar/',views.saludar),
+    path ('bienvenida/<str:nombre>/', views.bienvenida)
+
+]
---
+++ tercera_pre_entrega/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from datetime import datetime
+from django.template import Template, Context
+
+# V1
+
+#def inicio(request):
+#    return HttpResponse('Hola, Milan soy tu inicio')
+
+
+def inicio(request):
+    archivo = open(r'C:\Users\Irina\Desktop\Projectos\My_final_project\Tercera preentrega\tercera_pre_entrega\templates\inicio.html', 'r') 
+   
+
+    template = Template(archivo.read())
+    archivo.close()
+    contexto = Context()
+    
+    renderizar_template = template.render(contexto)
+    #return HttpResponse('Hola, Milan soy tu inicio')
+    return HttpResponse(renderizar_template)
+
+
+def segunda_vista(request):
+    return HttpResponse('<h1>Soy la segunda vista!</h1>')
+
+def fecha_actual(request):
+    fecha = datetime.now()
+    return HttpResponse(f'<h1>Fecha actual: {fecha}</h1>')
+
+def saludar(request):
+    return HttpResponse('Bienvenido/a!!!!')
+
+def bienvenida(request, nombre):
+    return HttpResponse(f'Bienvenido/a {nombre.title()}!!!!')
