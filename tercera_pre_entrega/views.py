fefbd62fe2058a6a4331211ce48f97de6c950454
Irina Greseva <iagreseva@gmail.com>
2023-06-28 23:04:00 -0300

    Projecto preentrega

--- tercera_pre_entrega/views.py
+++ tercera_pre_entrega/views.py
@@ -1,24 +1,57 @@
 from django.http import HttpResponse
 from datetime import datetime
-from django.template import Template, Context
+from django.template import Template, Context, loader
 
 # V1
 
 #def inicio(request):
 #    return HttpResponse('Hola, Milan soy tu inicio')
 
+# V2
+#def inicio(request):
+#    archivo = open(r'C:\Users\Irina\Desktop\Projectos\My_final_project\Tercera preentrega\tercera_pre_entrega\templates\inicio.html', 'r') 
+   
+    
+#    template = Template(archivo.read())
+#    archivo.close()
+   
+#    segundos =datetime.now().second
+#    diccionario = {
+#        'mensaje': 'Este es el mensaje de inicio....',
+#        'segundos': segundos,
+#        'segundo_par': segundos%2 == 0,
+#        'segundo_redondo': segundos%10 ==0,
+#        'listado_de_numeros': list(range(25))
+#    }
+#    contexto = Context(diccionario)
+    
+#    renderizar_template = template.render(contexto)
+    #return HttpResponse('Hola, Milan soy tu inicio')
+#    return HttpResponse(renderizar_template)
+
+#V3
 
 def inicio(request):
     archivo = open(r'C:\Users\Irina\Desktop\Projectos\My_final_project\Tercera preentrega\tercera_pre_entrega\templates\inicio.html', 'r') 
    
-
+    
     template = Template(archivo.read())
     archivo.close()
-    contexto = Context()
+   
+    segundos =datetime.now().second
+    diccionario = {
+        'mensaje': 'Este es el mensaje de inicio....',
+        'segundos': segundos,
+        'segundo_par': segundos%2 == 0,
+        'segundo_redondo': segundos%10 ==0,
+        'listado_de_numeros': list(range(25))
+    }
+    contexto = Context(diccionario)
     
     renderizar_template = template.render(contexto)
-    #return HttpResponse('Hola, Milan soy tu inicio')
+
     return HttpResponse(renderizar_template)
+
 
 
 def segunda_vista(request):
